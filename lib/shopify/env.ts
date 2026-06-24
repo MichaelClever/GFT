fddee40cd0247bcce62ba5cef9e053c754369d2b
MichaelClever <michaelclever@gmail.com>
2026-06-24 18:53:12 -0500

    Add explanatory comment for checkout feature flag

--- lib/shopify/env.ts
+++ lib/shopify/env.ts
@@ -1,3 +1,5 @@
 export const SHOPIFY_STORE_DOMAIN = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN;
 export const SHOPIFY_STOREFRONT_ACCESS_TOKEN = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+
+// To re-enable checkout, set NEXT_PUBLIC_CHECKOUT_ENABLED=true in the appropriate environment file and rebuild the site.
 export const CHECKOUT_ENABLED = process.env.NEXT_PUBLIC_CHECKOUT_ENABLED === 'true';
