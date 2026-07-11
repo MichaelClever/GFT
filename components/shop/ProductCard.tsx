df96d7c435cf6fd976885d8dee55a6feb742225a
MichaelClever <michaelclever@gmail.com>
2026-07-10 20:21:17 -0500

    Fix React.Fragment import in ProductCard.tsx

--- components/shop/ProductCard.tsx
+++ components/shop/ProductCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AddToCartButton } from './AddToCartButton';
 import { ProductSelection, ProductVariant } from '@/lib/shopify/productMap';
 
